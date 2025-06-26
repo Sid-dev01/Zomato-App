67a7c8461a0e6cfe353d636f3cf6bbd660967a1b
Siddhartha Gautam <sid.2005.19@gmail.com>
2025-06-26 19:59:04 +0530

    Completed Filter Page

--- Client/src/Pages/filter.js
+++ Client/src/Pages/filter.js
@@ -30,15 +30,15 @@ class Filter extends React.Component {
                     {/* Filter section  */}
                     <div className = "filter-section">
 
-                        <span class = "filters-text">Filters</span><br></br>
+                        <span class = "filters-text"><b>Filters</b></span><br></br>
 
-                        <span>Select Location</span><br></br>
+                        <span style={{color: "#192F60"}}>Select Location</span><br></br>
 
                         <select class="div-location" style={{marginBottom:"30px"}}>
                             <option value="0" disabled selected>Select Location</option>
                         </select>
                         <br></br>
-                        <span>Cuisine</span><br></br>
+                        <span style={{color: "#192F60"}}>Cuisine</span><br></br>
                         <label><input type="checkbox" />North Indian</label><br></br>
                         <label><input type="checkbox" />South Indian</label><br></br>
                         <label><input type="checkbox" />Chinese</label><br></br>
@@ -62,21 +62,90 @@ class Filter extends React.Component {
                     <div className = "div-card">
 
                         {/* Card 1  */}
-                        <div>
-                            <p>Yo</p>
+                        <div className="card" style={{marginBottom: "4 rem"}}>
+                            
+                            <div className="restaurantDetail">
+                                <div>
+                                    <img src="./Images/1.png" height="140px" width="140px" alt="mealtype_image" style={{borderRadius: "20px"}} />
+                                </div>
+                                <div style={{paddingRight: "3rem", marginLeft: "1rem"}}>
+                                    <h2>The Big Chill Cakery</h2>
+                                    <p>Fort</p>
+                                    <p>Shop 1, Plot D, Samruddhi Complex, Chincholi …</p>
+                                </div>
+                            </div>
+
+                            <div className="order-detail">
+                                <div className="div-data">
+                                    <div>
+                                        <p>CUISINIES:</p>
+                                    </div>
+                                    <div style={{paddingLeft:"6rem"}}>
+                                        <p>Bakery</p>
+                                    </div>
+                                </div>
+
+                                <div className="div-data"> 
+                                    <div>
+                                        <p>COST FOR TWO:</p>
+                                    </div>
+                                    <div style={{paddingLeft:"3rem"}}>
+                                        <p>₹700</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
 
                         {/* Card 2 */}
-                        <div>
-                            <p>Yo</p>
+                        <div className="card" >
+                            
+                            <div className="restaurantDetail">
+                                <div>
+                                    <img src="./Images/1.png" height="140px" width="140px" alt="mealtype_image" style={{borderRadius: "20px"}} />
+                                </div>
+                                <div style={{paddingRight: "3rem", marginLeft: "1rem"}}>
+                                    <h2>The Big Chill Cakery</h2>
+                                    <p>Fort</p>
+                                    <p>Shop 1, Plot D, Samruddhi Complex, Chincholi …</p>
+                                </div>
+                            </div>
+
+                            <div className="order-detail">
+                                <div className="div-data">
+                                    <div>
+                                        <p>CUISINIES:</p>
+                                    </div>
+                                    <div style={{paddingLeft:"6rem"}}>
+                                        <p>Bakery</p>
+                                    </div>
+                                </div>
+
+                                <div className="div-data"> 
+                                    <div>
+                                        <p>COST FOR TWO:</p>
+                                    </div>
+                                    <div style={{paddingLeft:"3rem"}}>
+                                        <p>₹700</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
 
+                        {/* Pagination  */}
+                    <div className="div-pagination">
+
+                        <div><button style={{padding:"0px 8px", borderRadius:"6px", marginRight:"10px"}}> 1</button></div>
+                        <div><button style={{padding:"0px 8px", borderRadius:"6px", marginRight:"10px"}}> 2</button></div>
+                        <div><button style={{padding:"0px 8px", borderRadius:"6px", marginRight:"10px"}}> 3</button></div>
+                        <div><button style={{padding:"0px 8px", borderRadius:"6px", marginRight:"10px"}}> 4</button></div>
+                        <div><button style={{padding:"0px 8px", borderRadius:"6px", marginRight:"10px"}}> 5</button></div>
+                        <div><button style={{padding:"0px 8px", borderRadius:"6px", marginRight:"10px"}}> 6</button></div>
+
                     </div>
 
-                    {/* Pagination  */}
-                    <div>
-
                     </div>
+
+                    
 
                 </div>
 
